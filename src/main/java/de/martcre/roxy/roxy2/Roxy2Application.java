689efb6fa162a73a68876e8fc33f34c3c85cac72
Martin Cremer <martin@abipolis.de>
2020-05-03 16:50:56 +0200

    springboot insert

---
+++ src/main/java/de/martcre/roxy/roxy2/Roxy2Application.java
@@ -0,0 +1,13 @@
+package de.martcre.roxy.roxy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roxy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roxy2Application.class, args);
+	}
+
+}
